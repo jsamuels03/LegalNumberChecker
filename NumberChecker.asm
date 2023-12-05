4fd528c4400d3d434c32058319a55a6907fcf0ab
Jaden Samuels <47543260+jsamuels03@users.noreply.github.com>
2023-12-04 23:40:04 -0500

    defined data area

--- NumberChecker.asm
+++ NumberChecker.asm
@@ -1,1 +1,5 @@
-
+.data
+    prompt_u: .asciiz "Enter an integer greater than 20: "
+    error_msg:  .asciiz "Illegal Number!\n"
+    gcdNote: .asciiz "The GCD of L, M, and N is: "
+    num: .space 12 # space for three integers
